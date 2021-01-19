b53f28c37ea648a2a0955f164492ad1d0f7abcf1
robinrrr10 <robinrrr10@gmail.com>
2021-01-19 22:47:25 +0530

    added login page

--- ordersui/urls.py
+++ ordersui/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('hm/', views.hm, name='hm'),
+    path('sign/', views.signin, name='signin'),
+    path('sign/login', views.login, name='login')
 ]
--- ordersui/views.py
+++ ordersui/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("Hello world")
+
+def hm(request):
+    nname = "Ram"
+    return render(request, 'index.html', { 'name': nname})
+
+def signin(request):
+    return render(request, 'sign.html')
+
+@csrf_exempt
+def login(request):
+    print("came inside..")
+    if request.method == 'POST':
+        return render(request, 'home.html')
+    else:
+        return HttpResponse("invalid request")

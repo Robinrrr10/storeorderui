d9e46a541efaad4e177c192cb9579df75d3e9977
root <root@docker.master.one>
2021-02-25 00:45:49 +0530

    modified docker requirement and settings

--- storeorderui/settings.py
+++ storeorderui/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+yeujy3+%$v)%8-&!xkvqlyl!_%%tc^x=nxcnjs)filz@rx+x3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
